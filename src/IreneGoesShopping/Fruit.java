e6221be725cd5064909927e210d8e0aa026e5fc9
yunw <ire.wang@gmail.com>
2013-01-08 19:58:14 -0800

    Update class to be compatible with jdk1.6.

--- src/IreneGoesShopping/Fruit.java
+++ src/IreneGoesShopping/Fruit.java
@@ -6,18 +6,25 @@ public class Fruit {
 	int quantity;
 	String fruitType;
 	
+	enum FruitName{
+		Apple, Balana, Berries, Orange;
+	}
+	
+	FruitName fname;
+	
 	public Fruit(String fruitType){
 		this.fruitType = fruitType;
 		this.name = fruitType;
 		
 	}
 	
-	float caculatePrice(String name, int quantity){
-		switch (name){
-		case "Apple": price = 1.02f; break;
-		case "Balana": price = 1.0f; break;
-		case "Berries": price = 2.1f; break;
-		case "Orange": price = 1.3f; break;
+	float caculatePrice(FruitName fname, int quantity){
+		Fruit f = new Fruit(fruitType);
+		switch (f.fname){
+		case Apple: price = 1.02f; break;
+		case Balana: price = 1.0f; break;
+		case Berries: price = 2.1f; break;
+		case Orange: price = 1.3f; break;
 		default: price = (float) 0; break;
 		}
 		
--- src/IreneGoesShopping/FunnyGurl.java
+++ src/IreneGoesShopping/FunnyGurl.java
@@ -1,5 +1,7 @@
 package IreneGoesShopping;
 
+import IreneGoesShopping.Fruit.FruitName;
+
 public class FunnyGurl {
 	
 	Fruit GroceriesBag[];
@@ -9,11 +11,14 @@ public class FunnyGurl {
 		irene.GroceriesBag[0] = new Fruit("Apple");
 		irene.GroceriesBag[1] = new Fruit("Banana");
 		
+		FruitName fn = null;
+
+		
 		System.out.println("Today Irene's grocessies bag has " + irene.GroceriesBag[0].fruitType
 				+ "and " + irene.GroceriesBag[1].fruitType);
 		System.out.print("She spent: ");
-		System.out.print(irene.GroceriesBag[0].caculatePrice("Apple", 6) + 
-				irene.GroceriesBag[1].caculatePrice("Banana", 2));
+		System.out.print(irene.GroceriesBag[0].caculatePrice(fn.Apple, 6) + 
+				irene.GroceriesBag[1].caculatePrice(fn.Balana, 2));
 	}
 
 	
