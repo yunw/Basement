4fbea6363152baacaf983ad26efeb2a09915ae0f
yunw <ire.wang@gmail.com>
2013-01-21 00:10:32 -0800

    update Bag

--- src/GrossayShopping/GrossaryBag.java
+++ src/GrossayShopping/GrossaryBag.java
@@ -1,13 +1,14 @@
 package GrossayShopping;
 
 public class GrossaryBag {
-	Fruit f;
+	Fruit[] f;
 
-	void addFruitToBag (Fruit f){
-		this.f = f;
+	void addFruitToBag (Fruit sf){
+		for (int i =0 ; i<f.length; i ++){
+		this.f[i] = sf;}
 		
 	}
-	Fruit getFruitsFromBag(){
+	Fruit[] getFruitsFromBag(){
 		return f;
 	}
 
