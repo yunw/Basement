3d0928ca585bb1761f02e046cf88dda60045613d
yunw <ire.wang@gmail.com>
2013-01-21 00:17:03 -0800

    add the bag max container for fruit

--- src/GrossayShopping/GrossaryBag.java
+++ src/GrossayShopping/GrossaryBag.java
@@ -4,10 +4,12 @@ public class GrossaryBag {
 	Fruit[] f;
 
 	void addFruitToBag (Fruit sf){
-		for (int i =0 ; i<f.length; i ++){
-		this.f[i] = sf;}
-		
+		int bagmax = 100;
+		for (int i=0 ; i<f.length || i < bagmax; i++)
+		{ this.f[i] = sf;
+		}
 	}
+	
 	Fruit[] getFruitsFromBag(){
 		return f;
 	}
